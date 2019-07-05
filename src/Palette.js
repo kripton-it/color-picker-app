8517504965f65e925b54b3895ea343c5383be892
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-05 15:29:02 +0500

    Заготовка компонента Palette

---
+++ src/Palette.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div className="Palette">
+        {/* Navbar will be here */}
+        <div className="Palette-colors">
+          {/* Color boxes will be here */}
+        </div>
+        {/* Footer will be here */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
