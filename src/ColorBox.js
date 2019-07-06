575bc4fd51e17819d6376f4748232965cceaf971
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-06 20:13:09 +0500

    Анимация оверлея после копирования в клипборд

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,45 @@
-import React, { Component } from 'react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import './ColorBox.css';
+import React, { Component } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./ColorBox.css";
 
 class ColorBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      copied: false
     };
   }
+
+  changeCopyState = () => {
+    this.setState(
+      {
+        copied: true
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({
+            copied: false
+          });
+        }, 3000);
+      }
+    );
+  };
+
   render() {
-    const {background, name} = this.props;
+    const { background, name } = this.props;
+    const { copied } = this.state;
+
     return (
-      <CopyToClipboard text={background}>
-        <div style={{backgroundColor: background}} className="ColorBox">
+      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+        <div style={{ backgroundColor: background }} className="ColorBox">
+          <div
+            className={`copy-overlay${copied ? " show" : ""}`}
+            style={{ backgroundColor: background }}
+          />
+          <div className={`copy-message${copied ? " show" : ""}`}>
+            <h1>Copied!</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
