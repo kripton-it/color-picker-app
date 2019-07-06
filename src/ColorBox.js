c2514135c4be40e9efe6aab1390cddc04ed28f96
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-06 18:56:53 +0500

    Готовый компонент CopyToClipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -9,19 +10,23 @@ class ColorBox extends Component {
     };
   }
   render() {
-    const {color, name} = this.props.background;
+    const {background, name} = this.props;
     return (
-      <div style={{backgroundColor: color}} className="ColorBox">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background}>
+        <div style={{backgroundColor: background}} className="ColorBox">
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <span className="see-more">More</span>
         </div>
-        <span className="see-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
 
 export default ColorBox;
+
+/* При желании, можно обернуть в CopyToClipboard только кнопку Copy */
--- src/Palette.js
+++ src/Palette.js
@@ -10,7 +10,7 @@ class Palette extends Component {
     };
   }
   render() {
-    const colorBoxes = this.props.colors.map(color => <ColorBox background={color} />)
+    const colorBoxes = this.props.colors.map(({color, name}) => <ColorBox background={color} name={name} />)
     return (
       <div className="Palette">
         {/* Navbar will be here */}
