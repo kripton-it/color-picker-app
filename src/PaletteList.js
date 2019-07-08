4201bf4e2e85c715329f45ab166d9d17f7119a2d
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-08 16:52:06 +0500

    Оформление компонента PaletteList с помощью CSS-in-JS

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,64 @@
 import React, { Component } from "react";
+import { withStyles } from "@material-ui/styles";
 import MiniPalette from "./MiniPalette";
+
+const styles = {
+  paletteList: {
+    backgroundColor: "blue",
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  container: {
+    outline: '1px solid red',
+    width: "50%",
+    display: 'flex',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+    // flexWrap: 'wrap',
+  },
+  navbar: {
+    display: 'flex',
+    // width: '100%',
+    justifyContent: 'space-between',
+    color: 'white',
+  },
+  list: {
+    listStyle: 'none',
+    margin: '0',
+    padding: '0',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '5%',
+    boxSizing: 'border-box',
+    // width: '100%',
+  }
+};
 
 class PaletteList extends Component {
   render() {
-    const {palettes} = this.props;
-    const paletteList = palettes.map(palette => <MiniPalette key={palette.id} {...palette} />);
+    const { palettes, classes } = this.props;
+    const { paletteList, container, navbar, list } = classes;
     // <Link to={`/palette/${id}`}>{paletteName}</Link>
 
-    return <div className="PaletteList">{paletteList}</div>;
+    return (
+      <div className={paletteList}>
+        <div className={container}>
+          <nav className={navbar}>
+            <h1>React Color Picker</h1>
+          </nav>
+          <ul className={list}>
+            {palettes.map(palette => (
+              <li key={palette.id}>
+                <MiniPalette {...palette} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
-export default PaletteList;
+export default withStyles(styles)(PaletteList);
