1b9dd12294288e68097e2fb23018f96072b6900f
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-08 15:02:02 +0500

    Базовый роутинг палитр

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Palette from './Palette';
-import seedColors from './seedColors';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Palette from "./Palette";
+import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
-  const palette = generatePalette(seedColors[1]);
+  // const palette = generatePalette(seedColors[1]);
+  const findPalette = id => seedColors.find(palette => palette.id === id);
+
   return (
     <div className="App">
-      <Palette {...palette}/>
+      <Switch>
+        {/* 
+          Здесь будет Route на главную страницу
+        <Route exact path="/" /> */}
+        <Route
+          exact
+          path="/palette/:id"
+          render={({match}) => (
+            <Palette {...generatePalette(findPalette(match.params.id))} />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
