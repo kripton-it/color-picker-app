0227ba3af85e1a4c97115ae1c5b75afd7c5af161
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-31 23:59:35 +0500

    Диалог PaletteMetaForm

--- src/NewPaletteFormNav.js
+++ src/NewPaletteFormNav.js
@@ -8,8 +8,8 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { withStyles } from "@material-ui/core/styles";
+import PaletteMetaForm from "./PaletteMetaForm";
 
 const drawerWidth = 400;
 // но лучше импортировать из одного места
@@ -44,86 +44,41 @@ const styles = theme => ({
   navBtns: {}
 });
 
-class NewPaletteFormNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      newPaletteName: ""
-    };
-  }
-
-  componentDidMount() {
-    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }
-
-  handleChange = ({ target }) => {
-    this.setState({
-      [target.name]: target.value
-    });
-  };
-
-  handleSubmit = () => {
-    this.props.handleSubmit(this.state.newPaletteName);
-  };
-
-  render() {
-    const { classes, open, handleDrawerOpen } = this.props;
-    const { newPaletteName } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          color="default"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open
-          })}
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Create a Palette
-            </Typography>
-          </Toolbar>
-          <div className={classes.navBtns}>
-            <ValidatorForm onSubmit={this.handleSubmit}>
-              <TextValidator
-                value={newPaletteName}
-                label="Palette Name"
-                name="newPaletteName"
-                onChange={this.handleChange}
-                validators={["required", "isPaletteNameUnique"]}
-                errorMessages={[
-                  "Palette name is required",
-                  "Palette name must be unique"
-                ]}
-              />
-              <Button variant="contained" color="primary" type="submit">
-                Save Palette
-              </Button>
-            </ValidatorForm>
-            <Link to="/">
-              <Button variant="contained" color="secondary">
-                Go Back
-              </Button>
-            </Link>
-          </div>
-        </AppBar>
-      </div>
-    );
-  }
-}
+const NewPaletteFormNav = ({ classes, open, handleDrawerOpen, handleSubmit, palettes }) => {
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="default"
+        className={classNames(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar disableGutters={!open}>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={handleDrawerOpen}
+            className={classNames(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" noWrap>
+            Create a Palette
+          </Typography>
+        </Toolbar>
+        <div className={classes.navBtns}>
+          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
+          <Link to="/">
+            <Button variant="contained" color="secondary">
+              Go Back
+            </Button>
+          </Link>
+        </div>
+      </AppBar>
+    </div>
+  );
+};
 
 export default withStyles(styles, { withTheme: true })(NewPaletteFormNav);
