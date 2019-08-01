8bba3d1100838cc4492ef81cf59e156aaafb3b4b
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-01 18:59:22 +0500

    Мелкие изменения стилей формы

--- src/NewPaletteFormNav.js
+++ src/NewPaletteFormNav.js
@@ -6,10 +6,10 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/core/styles";
 import PaletteMetaForm from "./PaletteMetaForm";
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
 
 import styles from "./styles/NewPaletteFormNavStyles";
 
@@ -72,9 +72,11 @@ class NewPaletteFormNav extends Component {
               color="inherit"
               aria-label="Open drawer"
               onClick={handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
+              className={classNames(classes.menuButton, {
+                [classes.hide]: open
+              })}
             >
-              <MenuIcon />
+              <AddToPhotosIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" noWrap>
               Create a Palette
