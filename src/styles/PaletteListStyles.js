91bd502f9f0e4eb9288f95286c0635f83de83d99
Mikhail Shardin <kriptonit1988@gmail.com>
2019-08-02 15:51:16 +0500

    Адаптивный список палитр

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from "../utils/mediaQueries";
+
 export default {
   paletteList: {
     backgroundColor: "blue",
@@ -12,6 +14,12 @@ export default {
     alignItems: 'stretch',
     flexDirection: 'column',
     // flexWrap: 'wrap',
+    [sizes.down("xl")]: {
+      width: "80%"
+    },
+    [sizes.down("xs")]: {
+      width: "75%"
+    }
   },
   navbar: {
     display: 'flex',
@@ -30,8 +38,15 @@ export default {
     padding: '0',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '5%',
+    gridGap: '2.5rem',
     boxSizing: 'border-box',
     // width: '100%',
+    [sizes.down("md")]: {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: 'repeat(1, 1fr)',
+      gridGap: '1rem',
+    },
   }
 };
